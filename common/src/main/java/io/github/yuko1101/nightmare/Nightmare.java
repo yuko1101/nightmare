3d24d9e2a2ffc51a1fd67b9067ab644dda02b889
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2025-02-04 20:21:02 +0900

    added more monsters to break blocks

--- common/src/main/java/io/github/yuko1101/nightmare/Nightmare.java
+++ common/src/main/java/io/github/yuko1101/nightmare/Nightmare.java
@@ -1,8 +1,26 @@
 package io.github.yuko1101.nightmare;
 
+import io.github.yuko1101.nightmare.entity.goals.BlockBreakGoal;
+import io.github.yuko1101.nightmare.utils.EntityUtils;
+import io.github.yuko1101.nightmare.utils.Vec3dUtils;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.world.Difficulty;
+
 public final class Nightmare {
     public static final String MOD_ID = "nightmare";
 
     public static void init() {
+    }
+
+    public static BlockBreakGoal createBlockBreakGoal(MobEntity mob) {
+        return new BlockBreakGoal(mob, difficulty -> difficulty == Difficulty.HARD, pos -> {
+            if (!pos.isWithinDistance(mob.getPos(), 4.0)) return false;
+            if (mob.getTarget() == null) return false;
+            if (!mob.getTarget().getBlockPos().isWithinDistance(mob.getPos(), 8.0)) return false;
+            if (Vec3dUtils.getAngle(mob.getPos(), mob.getTarget().getPos(), pos.toCenterPos()) > Math.PI / 2) return false;
+            BlockState blockState = mob.getWorld().getBlockState(pos);
+            return !blockState.isAir() && blockState.getHardness(mob.getWorld(), pos) < 100F; // TODO: better filtering
+        }, mobEntity -> mobEntity.getTarget() != null && mobEntity.getTarget().isAlive() && !EntityUtils.isRangedAttack(mobEntity));
     }
 }
--- common/src/main/java/io/github/yuko1101/nightmare/mixin/AbstractSkeletonEntityMixin.java
+++ common/src/main/java/io/github/yuko1101/nightmare/mixin/AbstractSkeletonEntityMixin.java
@@ -2,8 +2,8 @@ package io.github.yuko1101.nightmare.mixin;
 
 import io.github.yuko1101.nightmare.Nightmare;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,19 +11,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ZombieEntity.class)
-public abstract class ZombieEntityMixin extends HostileEntity {
-
-    protected ZombieEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+@Mixin(AbstractSkeletonEntity.class)
+public abstract class AbstractSkeletonEntityMixin extends HostileEntity {
+    protected AbstractSkeletonEntityMixin(EntityType<? extends AbstractSkeletonEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Inject(method = "initCustomGoals", at = @At("TAIL"))
+    @Inject(method = "initGoals", at = @At("TAIL"))
     private void initCustomGoals(CallbackInfo ci) {
         this.goalSelector.add(1, Nightmare.createBlockBreakGoal(this));
     }
 
-    @ModifyArg(method = "initCustomGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/ActiveTargetGoal;<init>(Lnet/minecraft/entity/mob/MobEntity;Ljava/lang/Class;Z)V"), index = 2)
+    @ModifyArg(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/ActiveTargetGoal;<init>(Lnet/minecraft/entity/mob/MobEntity;Ljava/lang/Class;Z)V"), index = 2)
     private boolean modifyChecks(boolean checkVisibility) {
         return false;
     }
